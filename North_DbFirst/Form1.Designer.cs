cb49bcd51f75dfe22d3ebe45a107bb081cbeae3c
Fulya Sarıyar Abdullah <fulya12agustos@hotmail.com>
2021-12-01 12:03:30 +0300

    SorgularDevam,SayfalamaYapildi.

--- North_DbFirst/Form1.Designer.cs
+++ North_DbFirst/Form1.Designer.cs
@@ -29,34 +29,73 @@
         private void InitializeComponent()
         {
             this.dgvNorth = new System.Windows.Forms.DataGridView();
+            this.btnGeri = new System.Windows.Forms.Button();
+            this.btnIleri = new System.Windows.Forms.Button();
+            this.lblSayfa = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvNorth)).BeginInit();
             this.SuspendLayout();
             // 
             // dgvNorth
             // 
             this.dgvNorth.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvNorth.Location = new System.Drawing.Point(12, 12);
+            this.dgvNorth.Location = new System.Drawing.Point(12, 33);
             this.dgvNorth.Name = "dgvNorth";
             this.dgvNorth.RowTemplate.Height = 25;
             this.dgvNorth.Size = new System.Drawing.Size(470, 400);
             this.dgvNorth.TabIndex = 0;
             // 
+            // btnGeri
+            // 
+            this.btnGeri.Location = new System.Drawing.Point(29, 4);
+            this.btnGeri.Name = "btnGeri";
+            this.btnGeri.Size = new System.Drawing.Size(75, 23);
+            this.btnGeri.TabIndex = 1;
+            this.btnGeri.Text = "<<<<<<";
+            this.btnGeri.UseVisualStyleBackColor = true;
+            this.btnGeri.Click += new System.EventHandler(this.btnGeri_Click);
+            // 
+            // btnIleri
+            // 
+            this.btnIleri.Location = new System.Drawing.Point(237, 4);
+            this.btnIleri.Name = "btnIleri";
+            this.btnIleri.Size = new System.Drawing.Size(75, 23);
+            this.btnIleri.TabIndex = 2;
+            this.btnIleri.Text = ">>>>>>";
+            this.btnIleri.UseVisualStyleBackColor = true;
+            this.btnIleri.Click += new System.EventHandler(this.btnIleri_Click);
+            // 
+            // lblSayfa
+            // 
+            this.lblSayfa.AutoSize = true;
+            this.lblSayfa.Location = new System.Drawing.Point(152, 9);
+            this.lblSayfa.Name = "lblSayfa";
+            this.lblSayfa.Size = new System.Drawing.Size(38, 15);
+            this.lblSayfa.TabIndex = 3;
+            this.lblSayfa.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 428);
+            this.ClientSize = new System.Drawing.Size(565, 447);
+            this.Controls.Add(this.lblSayfa);
+            this.Controls.Add(this.btnIleri);
+            this.Controls.Add(this.btnGeri);
             this.Controls.Add(this.dgvNorth);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvNorth)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private DataGridView dgvNorth;
+        private Button btnGeri;
+        private Button btnIleri;
+        private Label lblSayfa;
     }
 }
--- North_DbFirst/Form1.cs
+++ North_DbFirst/Form1.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using North_DbFirst.Models;
 using North_DbFirst.Models.ViewModel;
 
@@ -9,9 +10,11 @@ namespace North_DbFirst
         {
             InitializeComponent();
         }
+
         private NorthwindContext _dbContext = new NorthwindContext();
         private void Form1_Load(object sender, EventArgs e)
         {
+            
             var query1 = _dbContext.Categories.Select(x => new CategoryViewModel()
             {
                 CategoryName = x.CategoryName,
@@ -42,6 +45,103 @@ namespace North_DbFirst
             }).OrderBy(x => x.CategoryName).ThenByDescending(x => x.UnitPrice).ToList();
             dgvNorth.DataSource = query3;
 
+            var query4 = _dbContext.Products
+                .Include(x => x.Supplier)
+                .Include(x => x.Category)
+                .Select(x => new
+                {
+                    x.ProductName,
+                    x.UnitPrice,
+                    x.Supplier.CompanyName,
+                    x.Category.CategoryName
+
+                }).ToList();
+
+            var query5 = _dbContext.Products
+                .Include(x => x.Supplier)
+                .Include(x => x.Category)
+                .Include(x => x.OrderDetails)
+                .ThenInclude(x => x.Order)
+                .ThenInclude(x => x.ShipViaNavigation);
+            //.Select(x => new
+            //{
+            //    x.ProductName,
+            //    x.Category.CategoryName,
+            //    x.Supplier.CompanyName,
+            //    x.OrderDetails.Count
+
+            //})
+            //.ToList();
+            var queryString = query5.ToQueryString(); //sql'e giden sorguyu gormek icin. Tolist yapmadan gorebýlýrsýn.
+            var qq = query5.ToList();
+
+
+            var query6 = _dbContext.Products //tum kayitlar yerine istedigim sayýda kaydi cekememiz icin.
+                .Include(x => x.Category)
+                .Include(x => x.Supplier)
+                .Select(x => new
+                {
+                    x.Category.CategoryName,
+                    x.ProductName,
+                    x.Supplier.CompanyName
+
+                })
+                .OrderBy(x => x.CategoryName)
+                .Skip(_offset * _pageSize)
+                .Take(_pageSize)
+                .ToList();
+
+            RaporuGoster();
+            Console.WriteLine();
+
+        }
+        private int _offset = 0, _pageSize = 10, _maxPage = 0;
+
+        private void btnIleri_Click(object sender, EventArgs e)
+        {
+            if (_offset + 1 == _maxPage)
+            {
+                return;
+            }
+            _offset++;
+            RaporuGoster();
+        }
+
+        private void btnGeri_Click(object sender, EventArgs e)
+        {
+            if (_offset == 0)
+            {
+                return;
+            }
+            _offset--;
+            RaporuGoster();
+        }
+        private void RaporuGoster()
+        {
+            lblSayfa.Text = $"{_offset + 1}";
+            var query = _dbContext.Products
+                .Include(x => x.Category)
+                .Include(x => x.Supplier)
+                .Select(x => new
+                {
+                    x.Category.CategoryName,
+                    x.ProductName,
+                    x.UnitPrice,
+                    x.Supplier.CompanyName
+
+
+                });
+            if (_maxPage == 0)
+            {
+                _maxPage = (int)Math.Ceiling(query.Count() / Convert.ToDouble(_pageSize));
+            }
+            var result = query
+                .OrderBy(x => x.CategoryName)
+                .Skip(_offset * _pageSize)
+                .Take(_pageSize)
+                .ToList();
+            dgvNorth.DataSource = result;
+            dgvNorth.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
         }
     }
 }
